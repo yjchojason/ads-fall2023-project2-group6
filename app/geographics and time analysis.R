07eeebcb39cc643f24926317094248bb2e7adc65
brahmbhatt1999@gmail.com <brahmbhatt1999@gmail.com>
2023-10-11 10:43:16 -0400

    update github

--- app/geographics and time analysis.R
+++ app/geographics and time analysis.R
@@ -1,12 +1,36 @@
 
 
-dis <- read.csv("/Users/wshmac/Documents/GitHub/ads-fall2023-project2-group6/data/Disaster_data.csv")
-url <- "/Users/wshmac/Documents/GitHub/ads-fall2023-project2-group6/data/MajorDisaster_data.csv"
-#geographic analysis 
+dis <- read.csv("/Users/mannyb/Documents/GitHub/ads-fall2023-project2-group6/data/Disaster_data.csv")
+url <- "/Users/mannyb/Documents/GitHub/ads-fall2023-project2-group6/data/MajorDisaster_data.csv"
 
-library(shiny) 
-library(usmap)
+if (!require("DT")) install.packages('DT')
+if (!require("dtplyr")) install.packages('dtplyr')
+if (!require("lubridate")) install.packages('lubridate')
+if (!require("ggmap")) install.packages('ggmap')
+if (!require("choroplethrZip")) {
+  #install.packages("devtools")
+  library(devtools)
+  install_github('arilamstein/choroplethrZip@v1.5.0')}
+
+if (!require("USA.state.boundaries")) install.packages('USA.state.boundaries')
+if (!require("dygraphs")) install.packages('dygraphs')
+if (!require("dplyr")) install.packages('dplyr')
+if (!require("usmap")) install.packages('usmap')
+
+
+library(dtplyr)
+library(dplyr)
+library(DT)
+library(lubridate)
 library(ggplot2)
+library(shiny)
+library(USA.state.boundaries)
+library(leaflet)
+library(viridis)
+library(DT)
+library(dygraphs)
+library(tidyr)
+library(usmap)
 
 incidentType <- unique(dis$incidentType)
 states <- unique(dis$state)
@@ -15,6 +39,7 @@ all
 specific <- data.frame(incidentType = character(0), state = character(0))
 
 # generate table with info of state, disasters and # of disasters
+
 for (stateName in states) {
   for (disas in incidentType) {
     subset_data <- subset(dis, state == stateName & incidentType == disas)
@@ -23,6 +48,28 @@ for (stateName in states) {
   }
 }
 specific[is.na(specific$count), "count"] <- 0 # fill na
+
+
+
+
+#time analysis
+
+
+major_disaster_data <- read.csv(url)
+
+#head(major_disaster_data) 
+unique(major_disaster_data$incidentType)
+
+#declarationDate -> Date format
+major_disaster_data$declarationDate <- as.Date(major_disaster_data$declarationDate, format="%Y-%m-%d")
+
+# Extracting Year and Month
+major_disaster_data$Year <- as.numeric(format(major_disaster_data$declarationDate, "%Y"))
+major_disaster_data$Month <- as.numeric(format(major_disaster_data$declarationDate, "%m"))
+
+
+
+
 
 # define the heatmap function
 plot_disaster_heatmap <- function(disaster_name) {
@@ -41,6 +88,7 @@ plot_disaster_heatmap <- function(disaster_name) {
 }
 
 
+#Geospatial Analysis
 
 ui1 <- fluidPage( 
   titlePanel("Disaster Geographics"),
@@ -55,7 +103,7 @@ ui1 <- fluidPage(
                     'Volcanic Eruption', 'Freezing', 'Toxic Substances', 'Chemical',
                     'Terrorist', 'Drought', 'Human Cause', 'Fishing Losses', 'Tsunami'), selected = 1),
       
-      uiOutput("tooltip_text") #add an element to show the UI output
+      uiOutput("tooltip_text")
     ),
     mainPanel(
       verbatimTextOutput("disaster_chosen"),
@@ -65,71 +113,7 @@ ui1 <- fluidPage(
 )
 
 
-server1 <- function(input, output, session){
-  tooltips <- c(
-    'All','Fire', 'Severe Storm', 'Winter Storm', 'Flood', 'Tornado',
-    'Snowstorm', 'Earthquake', 'Biological', 'Hurricane',          
-    'Mud/Landslide', 'Coastal Storm', 'Other', 'Severe Ice Storm',
-    'Dam/Levee Break', 'Tropical Storm', 'Typhoon',
-    'Volcanic Eruption', 'Freezing', 'Toxic Substances', 'Chemical',
-    'Terrorist', 'Drought', 'Human Cause', 'Fishing Losses', 'Tsunami'
-  )
-  
-  output$tooltip_text <- renderUI({
-    tooltip <- tooltips[input$select]
-    HTML(paste("<div data-toggle='tooltip' data-placement='bottom' title='", tooltip, "'>Hover for info</div>"))
-  })
-  
-  output$heatmap <- renderPlot({
-    disaster_type <- input$select
-    plot_disaster_heatmap(disaster_type) # Removed print here
-  })
-  
-  output$disaster_chosen <- renderText({
-    paste("The disaster you choose is:", input$select) # Use input$select directly
-  })
-}
-
-#time analysis
-knitr::opts_chunk$set(fig.width=12, fig.height=8, fig.path='figs/',
-                      echo=T, warning=FALSE, message=FALSE)
-
-if (!require("DT")) install.packages('DT')
-if (!require("dtplyr")) install.packages('dtplyr')
-if (!require("lubridate")) install.packages('lubridate')
-if (!require("ggmap")) install.packages('ggmap')
-if (!require("choroplethrZip")) {
-  # install.packages("devtools")
-  library(devtools)
-  install_github('arilamstein/choroplethrZip@v1.5.0')}
-
-library(dtplyr)
-library(dplyr)
-library(DT)
-library(lubridate)
-library(ggplot2)
-library(shiny)
-library(tidyr)
-
-
-major_disaster_data <- read.csv(url)
-
-#head(major_disaster_data) 
-unique(major_disaster_data$incidentType)
-
-#declarationDate -> Date format
-major_disaster_data$declarationDate <- as.Date(major_disaster_data$declarationDate, format="%Y-%m-%d")
-
-# Extracting Year and Month
-major_disaster_data$Year <- as.numeric(format(major_disaster_data$declarationDate, "%Y"))
-major_disaster_data$Month <- as.numeric(format(major_disaster_data$declarationDate, "%m"))
-
-#Clean up Data to do state-wise
-state_data <- major_disaster_data %>%
-  select(state, declarationDate, Year, Month, disasterNumber, incidentType) %>%
-  distinct(disasterNumber, .keep_all = TRUE)
-
-## Yearly and Monthly Analysis
+#Time Analysis 1 - Yearly, Seasonal, Average Duration
 
 # Define UI
 ui2<- fluidPage(
@@ -152,6 +136,7 @@ ui2<- fluidPage(
                     max = max(major_disaster_data$Year),
                     value = max(major_disaster_data$Year),
                     step = 1,
+                    sep = "",
                     round = TRUE)
       )
     ),
@@ -161,8 +146,63 @@ ui2<- fluidPage(
   )
 )
 
-# SERVER
-server2 <- function(input, output) {
+
+#Time Analysis 2 - Most Frequent Disaster by State and Year
+
+stateShapes <- state_boundaries_wgs84
+
+ui3 <- fluidPage(
+  titlePanel("Most Frequent Incident Type Analysis by State"),
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("yearSlider_second", "Select Year:",
+                  min = min(major_disaster_data$Year),
+                  max = max(major_disaster_data$Year),
+                  value = min(major_disaster_data$Year),
+                  step = 1,
+                  round = TRUE,
+                  sep = "")
+    ),
+    mainPanel(
+      leafletOutput("map"),
+      DTOutput("stateData")
+    )
+  )
+)
+
+ui <- navbarPage("Combined Shiny App",
+                 tabPanel("Disaster Geographics", ui1),
+                 tabPanel("Major Disaster Data Analysis", ui2),
+                 tabPanel("Most Frequent Incident Type Analysis", ui3)
+)
+
+server <- function(input, output, session) {
+  
+  # Logic for the first app
+  tooltips <- c(
+    'All','Fire', 'Severe Storm', 'Winter Storm', 'Flood', 'Tornado',
+    'Snowstorm', 'Earthquake', 'Biological', 'Hurricane',
+    'Mud/Landslide', 'Coastal Storm', 'Other', 'Severe Ice Storm',
+    'Dam/Levee Break', 'Tropical Storm', 'Typhoon',
+    'Volcanic Eruption', 'Freezing', 'Toxic Substances', 'Chemical',
+    'Terrorist', 'Drought', 'Human Cause', 'Fishing Losses', 'Tsunami'
+  )
+  
+  output$tooltip_text <- renderUI({
+    tooltip <- tooltips[input$select]
+    HTML(paste("<div data-toggle='tooltip' data-placement='bottom' title='", tooltip, "'>Hover for info</div>"))
+  })
+  
+  output$heatmap <- renderPlot({
+    disaster_type <- input$select
+    plot_disaster_heatmap(disaster_type)
+  })
+  
+  output$disaster_chosen <- renderText({
+    paste("The disaster you chose is:", input$select)
+  })
+  
+  # Logic for the second app
   output$disasterPlot <- renderPlot({
     filtered_data <- major_disaster_data
     
@@ -182,15 +222,15 @@ server2 <- function(input, output) {
       
     } else if (input$analysisType == "Seasonal Analysis") {
       all_months <- data.frame(Month = 1:12)
-      monthly_data <- filtered_data %>% 
-        filter(Year == input$yearSlider) %>% 
-        group_by(Month) %>% 
+      monthly_data <- filtered_data %>%
+        filter(Year == input$yearSlider) %>%
+        group_by(Month) %>%
         summarise(Count = n())
-      monthly_data <- all_months %>% 
-        left_join(monthly_data, by = "Month") %>% 
+      monthly_data <- all_months %>%
+        left_join(monthly_data, by = "Month") %>%
         replace_na(list(Count = 0))
       monthly_data$Month <- factor(monthly_data$Month, levels = 1:12, labels = month.abb)
-      ggplot(monthly_data, aes(x=Month, y=Count)) + geom_bar(stat="identity") + 
+      ggplot(monthly_data, aes(x=Month, y=Count)) + geom_bar(stat="identity") +
         labs(title=paste("Monthly", input$disasterType, "Declarations for", input$yearSlider))
       
     } else { # Average Duration Analysis
@@ -204,34 +244,11 @@ server2 <- function(input, output) {
         labs(title = "Average Duration of Disasters by Type", y = "Average Duration (days)", x = "Disaster Type")
     }
   })
-}
-
-stateShapes <- state_boundaries_wgs84
-
-ui3 <- fluidPage(
-  titlePanel("Most Frequent Incident Type Analysis by State"),
-  sidebarLayout(
-    sidebarPanel(
-      sliderInput("yearSlider", "Select Year:",
-                  min = min(major_disaster_data$Year),
-                  max = max(major_disaster_data$Year),
-                  value = min(major_disaster_data$Year),
-                  step = 1,
-                  round = TRUE,
-                  sep = "")
-    ),
-    mainPanel(
-      leafletOutput("map"),
-      DTOutput("stateData")
-    )
-  )
-)
-
-server3 <- function(input, output) {
   
+  # Logic for the third app
   output$map <- renderLeaflet({
     data_year <- major_disaster_data %>%
-      filter(Year == input$yearSlider) %>%
+      filter(Year == input$yearSlider_second) %>%
       group_by(state) %>%
       count(incidentType, sort = TRUE)
     
@@ -253,8 +270,8 @@ server3 <- function(input, output) {
   })
   
   output$stateData <- renderDT({
-    data_year <- major_disaster_data %>% 
-      filter(Year == input$yearSlider) %>%
+    data_year <- major_disaster_data %>%
+      filter(Year == input$yearSlider_second) %>%
       select(state, declarationDate, incidentType) %>%
       arrange(state)
     
@@ -263,31 +280,5 @@ server3 <- function(input, output) {
   
 }
 
-ui <- fluidPage(
-  tabsetPanel(
-    tabPanel('app 1', ui1),
-    tabPanel('app 2', ui2),
-    tabPanel('app 3', ui3)
-  )
-)
-
-server <- function(input, output) {
-  output$app1 <- renderUI({
-    ui1
-  })
-  
-  output$app2 <- renderUI({
-    ui2
-  })
-  
-  output$app3 <- renderUI({
-    ui3
-  })
-  
-  # Include the server logic for both apps within the main server function
-  server1(input, output)
-  server2(input, output)
-  server3(input, output)
-}
 
 shinyApp(ui, server)
